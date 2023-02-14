9e1c094811d88b1807cd7d56e6bed094e885c76a
sachindaMass <sachinda46@gmail.com>
2023-02-14 16:24:08 +0530

    testing h2 database with springboot

---
+++ src/main/java/com/example/entity/entity/entity/Product.java
@@ -0,0 +1,56 @@
+package com.example.entity.entity.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Product{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String category;
+    private String type;
+    private String sector;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
+}
